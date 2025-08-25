5d07805091db173b81f2e3b0fe123d88e5d92d8d
Carolina Herrera <carolinahradiaz@gmail.com>
2025-08-25 21:42:21 +0200

    added new property reference

--- src/Models/Loan.php
+++ src/Models/Loan.php
@@ -4,6 +4,7 @@ readonly class Loan {
     public function __construct(
         public int $id,
         public int $memberId,
+        public string $reference,
         public int $state,
         public string $dueDate,
         public string $createdAt,
