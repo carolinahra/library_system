ab04514580d5934e6049747e899963c28152242b
Carolina Herrera <carolinahradiaz@gmail.com>
2025-08-25 20:34:38 +0200

    created Loan Model

---
+++ src/Models/Loan.php
@@ -0,0 +1,12 @@
+<?php
+namespace Models;
+readonly class Loan {
+    public function __construct(
+        public int $id,
+        public int $memberId,
+        public int $state,
+        public string $dueDate,
+        public string $createdAt,
+        public string $updatedAt
+        ){}
+}
