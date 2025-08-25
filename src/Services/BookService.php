23acb3ab67aedc597dda119ea72254c77dbe5b3a
Carolina Herrera <carolinahradiaz@gmail.com>
2025-08-25 20:24:53 +0200

    finished book service

--- src/Services/BookService.php
+++ src/Services/BookService.php
@@ -9,22 +9,22 @@ readonly class BookService
 {
     public function __construct(private BookFactory $bookFactory) {}
 
-    public function get(?int $id = null, ?string $ISBN = null, ?string $name = null, ?int $state = null, ?int $limit = null, ?int $offset = null): Book
+    public function get(?int $id = null, ?string $ISBN = null, ?string $name = null, ?int $state = null, ?int $limit = null, ?int $offset = null): Book|array
     {
         return $this->bookFactory->get($id, $ISBN, $name, $state, $limit, $offset);
     }
 
-    public function insert(string $ISBN, string $name, int $stock, int $state)
+    public function insert(string $ISBN, string $name, int $stock, int $state): Book
     {
         return $this->bookFactory->insert($ISBN, $name, $stock, $state);
     }
 
-    public function update(string $ISBN, ?string $name = null, ?int $state = null)
+    public function update(string $ISBN, ?string $name = null, ?int $state = null): Book
     {
         return $this->bookFactory->update($ISBN, $name, $state);
     }
 
-    public function delete(string $ISBN) {
+    public function delete(string $ISBN): bool {
         return $this->bookFactory->delete($ISBN);
     }
 }
